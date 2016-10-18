f40e3746c13c688603709f4fa4b07bc095229a26
allx1m1k <dima@ua2012.org.ua>
2016-10-19 02:58:55 +0300

    1.Компилятор бростает wrong identifier Child 2. Как засетапить вектор Child в Parent 3. Какие использовать Конструкторы Child

---
+++ OOP/OOP/oop.cpp
@@ -0,0 +1,12 @@
+#include "oop.h"
+#include <vector>
+#include <string>
+#include <initializer_list>
+
+using namespace std;
+
+int main()
+{
+	Parent* bobParent = new Parent("Bob", 40);
+	
+}
---
+++ OOP/OOP/oop.h
@@ -0,0 +1,63 @@
+#pragma once
+
+#include <vector>
+#include <string>
+#include <initializer_list>
+
+class Parent
+{ //
+public:
+	Parent();  //default constructor
+	Parent(std::string name, int age);  //constructor with parametr
+	~Parent(); //destructor
+	void eat();
+	void sleep();
+	void drink();
+	bool hasChildren; //number of Children
+	void addChildren(std::vector<std::string> children);
+	const std::vector<std::string> mChildren; 
+	//const std::vector<Child> mChildren; //NOK Parent's children, this is vector of refs of Child types
+	//void addChildren(Child& children);
+
+protected:
+	std::string mParentName; //Parent name
+	uint16_t mAge; //Parent age
+
+}; //end of Parent
+
+//Parent default constructor
+Parent::Parent()
+	: hasChildren(false)
+	, mParentName("NAN")
+	, mAge(0)
+	, mChildren(0)
+{}
+
+//Parent constructor with formal arument
+Parent::Parent(std::string name, int age)
+	: hasChildren(0)
+	, mParentName(name)
+	, mAge(age)
+	, mChildren(0)
+{}
+
+//Parent instance method
+void addChildren(std::vector<std::string> children)
+{
+
+}
+
+//Child is ancestor of Parent 
+class Child : public Parent
+{
+public:
+	Child();  //default constructor
+	~Child(); //destructor
+
+}; //end of Child
+
+
+
+
+
+
