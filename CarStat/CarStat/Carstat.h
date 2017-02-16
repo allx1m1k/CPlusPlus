a7828c329a8f1366c0ea048a3dd6ebae2f45cab2
allx1m1k <dima@ua2012.org.ua>
2017-02-17 00:14:38 +0200

    Added notes

--- CarStat/CarStat/Carstat.h
+++ CarStat/CarStat/Carstat.h
@@ -15,9 +15,11 @@
 //.в с++11 встроен удобный api для работы с файлами - используй его
 //https://www.tutorialspoint.com/cplusplus/cpp_files_streams.htm
 //fstream https://msdn.microsoft.com/en-us/library/k8w40w6t.aspx
-//. по-строчная вычитка из файла по условию (год выпуска <= заданный год)
+//. вычитка структуры из файла http://ci-plus-plus-snachala.ru/?p=2177
+//.по-сточная вычитка из файла по условию (год выпуска <= заданный год)
 //см. тут http://www.cplusplus.com/forum/beginner/208388/#msg982326
-//. запись считанной строки в несортированную динамическую структуру-контейнер
+//. вычитка в буфер http://www.programmersforum.ru/showthread.php?t=91445
+//запись считанной строки в несортированную динамическую структуру-контейнер
 //см. stl контейнеры http://informatics.mccme.ru/mod/book/view.php?id=492&chapterid=200
 //. сортировка динамических контейнеров http://www.cplusplus.com/articles/NhA0RXSz/ 
 
